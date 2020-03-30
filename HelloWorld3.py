ccbd1ee1fa876c8214ffbf247d0a5b2ab7bade5e
DougTaft <dogincolorado@hotmail.com>
2020-03-30 17:05:52 -0600

    blah

---
+++ HelloWorld3.py
@@ -0,0 +1,13 @@
+print('Hello World')
+# print('What is your name?')
+# myName = input()
+# print('Hello ' + myName + ' the length of your name is: ' + str(len(myName)))
+# print(len(myName))
+# xi = int('99.99')
+# print(xi)
+
+total = 0
+for num in range(101):
+    total += num
+    print(total)
+print(total)
